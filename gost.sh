b2f2ffc885e0bbf394dfc7bd3cf29dc54430b396
ChenAIAIOAI <59353619+ChenAIAIOAI@users.noreply.github.com>
2020-08-26 21:59:26 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -105,7 +105,7 @@ function Install_ct()
     `mv gost-linux-"$bit"-"$ct_new_ver" gost`
     `mv gost /usr/bin/gost`
     `chmod -R 777 /usr/bin/gost`
-    `wget --no-check-certificate https://raw.githubusercontent.com/stsdust/EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system`
+    `wget --no-check-certificate https://raw.githubusercontent.com/ChenAIAIOAI/EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system`
     `mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/stsdust/EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost`
     `systemctl enable gost && systemctl restart gost`
     echo "------------------------------"
